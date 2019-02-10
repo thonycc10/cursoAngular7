6a156834325109563ba30513c3d67da40c90c6b0
BC5415 <anthony.carrasco@skf.com>
2019-02-09 23:49:17 -0500

    AC: paginator ngonchamges

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,17 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import index from '@angular/cli/lib/cli';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
  @Input() paginador: any;
  paginas: number[];
+ desde: number;
+ hasta: number;
+
   constructor() { }
 
   ngOnInit() {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, i) => i + 1);
+  }
+
+  ngOnChanges() {
+    this.desde = Math.min( Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
+    this.hasta =  Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+    if (this.paginador.totalPages > 5) {
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((_valor, i) => i + this.desde);
+    } else {
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, i) => i + 1);
+    }
   }
 
 }
